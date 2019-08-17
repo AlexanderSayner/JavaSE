879733f0787cbc7f79440e7b1019453c6bda9304
Void Archon <voidarchon@void.sayner.ru>
2019-08-17 19:44:23 +0300

    В проект добавлены два модуля: один исполняемый, другой - библиотечный

---
+++ Loader/src/main/java/sayner/sandbox/liba/Simple.java
@@ -0,0 +1,14 @@
+package sayner.sandbox.liba;
+
+public class Simple {
+
+    private int value;
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
